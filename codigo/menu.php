b33a9acd3ad47750c789262e4f3be62eeba8599a
Alexia Fernanda <alexiafernanda718@gmail.com>
2023-02-11 12:24:21 -0300

    Modifiquei a pasta codigo, adicionei estilo2.css e adicionei imgs

--- codigo/menu.php
+++ codigo/menu.php
@@ -1,10 +1,63 @@
-<header>
-        <h1>FAVAI Tech</h1>
-        <p>Eletroeletrônicos e suas consequÊncias para o meio ambiente </p>
-    </header>
-    <nav>
-        <a href="index.php">Home</a>
-        <a href="#">...</a>
-        <a href="./sistema-cadastro-php-main/login.php">Cadastrar/Entrar</a>
-        <a href="#">Informações</a>
-    </nav>
+
+
+        <!-- Bootstrap CSS -->
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="../estilo/estilo2.css">
+        <link href="dist/aos.css" rel="stylesheet">
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.3/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-147081812-3"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-147081812-3');
+        </script>
+           <div class="container">
+               <div class="col-md-12">
+                <nav class="navbar navbar-default navbar-fixed-top">
+                    <div class="container menu">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            </button>
+                            <a href="#topo" class="link-topo">
+                                <img src="../img/favicon.ico" class="img-responsive logo" alt="">
+                            </a>
+                        </div>
+
+                        <!-- Collect the nav links, forms, and other content for toggling -->
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul class="nav navbar-nav">
+                            <li><a href="#conheca">Histórias <br>reais</a></li>
+                            <li><a href="#numeros">Nossos <br>resultados</a></li>
+                            <li><a href="#oque">O que a FAVAI <br>faz por você?</a></li>
+                            <li><a href="#euvalorizo">#euValorizo</a></li>
+                            <li><a href="#transparencia">Transparência</a></li>
+                            <li><a href="#euapoio">Eu apoio</a></li>
+                            <li><a href="#contato">Contato</a></li>
+                            
+                            </ul>
+                        </div>
+                      </div>
+                    </div>
+                    <br><br><br><br>
+                    <div class="col-md-12" id="topo">
+                        <img src="../img/natureza,jpg.webp" class="center-block pessoas" data-aos="fade" data-aos-delay="300" alt="">
+                    </div>
+                  </nav>
+               </div>
+           </div>
+           <br><br>
+                   

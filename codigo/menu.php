857c7173c5d65d13b6e74a5be88b1d8c44e5ffab
Alexia Fernanda <alexiafernanda718@gmail.com>
2023-02-11 09:26:23 -0300

    Adicionando arquivos

---
+++ codigo/menu.php
@@ -0,0 +1,10 @@
+<header>
+        <h1>Site Do Professor Julio</h1>
+        <p>Site desenvolvido para alunos do professor Julio. Entre com seu usuário e senha!</p>
+    </header>
+    <nav>
+        <a href="Julio.php">Home</a>
+        <a href="templates/index.html">Turma</a>
+        <a href="./sistema-cadastro-php-main/login.php">Cadastrar/Entrar</a>
+        <a href="#">Informações</a>
+    </nav>

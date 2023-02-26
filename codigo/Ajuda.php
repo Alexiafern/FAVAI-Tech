5bf30d6ac569954690b39b9da47a27917333a417
Alexia Fernanda <alexiafernanda718@gmail.com>
2023-02-26 16:30:47 -0300

    Adicionando novos arquivos e modificando os já existentes

--- codigo/Ajuda.php
+++ codigo/Ajuda.php
@@ -1,3 +1,8 @@
+<?php
+
+    include('menu.php')
+
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -5,9 +10,71 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../estilo/estilo2.css">
+    <style>
+        .img-container {
+          text-align: center;
+          display: block;
+        }
+        .mein{
+            background-color: #c5ebd6;
+            padding: 10px;
+            border-radius: 10px;
+            box-shadow: 3px 3px 8px rgba(0, 0, 0, 0.212);
+            list-style-position: inside;
+            columns: 1;
+        }
+        </style>
+        
 </head>
 <body>
-    <h1>EM DESENVOLVIMENTOOOOOOOOO!!!!!!!!!!!!!</h1>
+    <main>
+        <article>
+            <br>
+            <div class="col-md-12" id="topo">
+                        <img src="../img/reciclagem_do_e-lixo.webp" class="center-block pessoas" data-aos="fade" data-aos-delay="300" alt="" width="25px">
+                        <br>
+                    </div>
+            <h1>Como Descartar Corretamente O Lixo Eletrônico Da Sua Empresa?</h1>
+
+            <p>Em pleno 2023, é muito improvável encontrar uma empresa que não tenha lixo eletrônico, também conhecidos como Resíduos de Equipamentos Elétricos e Eletrônicos (REEE) a serem descartados, desde celulares antigos e impressoras que não funcionam mais a até computadores ultrapassados toda empresa em algum momento irá ter de lidar com isso.</p>
+            <span class="img-container"><a href="problemas.php"><img src="../img/impactos.webp" alt=""></a></span>
+            
+            <p>E sabemos que muitas vezes o descarte é adiado por simplesmente não saber e/ou achar muito complicado realizá-lo, o que não é verdade. Isso porque, por todo o estado, existem diversos pontos para descarte de lixo eletrônico e a FAVAI-Tech é um deles!</p>
+
+            <p>Portanto este tipo de lixo não deve ser jogado no lixo comum, você pode participar sendo um multiplicador da informação e do conhecimento, ajudando para que o e-lixo seja encaminhado para reciclagem na FAVAI-Tech.</p>
+
+            <h2>Mas o que pode ser considerado lixo eletrônico?</h2>
+
+            <p><strong>Grandes equipamentos:</strong> geladeiras, freezers, máquinas de lavar, ar condicionado, microondas, TVs, etc.</p>
+
+            <p><strong>Pequenos equipamentos e eletroportáteis:</strong> câmeras digitais, torradeiras, batedeiras, aspiradores de pó, ventiladores, secadores de cabelo, ferramentas elétricas, calculadoras, etc.</p>
+
+            <p><strong>Equipamentos de informática e telefonia:</strong> CPUs, tablets, notebooks, celulares, impressoras, monitores e entre outros.</p>
+
+            <p>Aparelhos que estão presentes nos mais diversos segmentos, desde fábricas e indústrias, até comércios e escritórios.</p>
+            <h2>E quais são as vantagens de realizar o descarte correto do lixo eletrônico? </h2>
+            <p>Os benefícios são inúmeros!</p>
+            <p>Principalmente para a natureza, confira abaixo alguns deles: </p>
+            <div class="mein">
+                <ul>
+                    <p><strong>– Redução de lixo nos aterros sanitários</strong></p>
+                    <p>Segundo a ONU, somente no Brasil são mais de 1,5 milhão de toneladas de resíduos eletrônicos descartadas anualmente!</p>
+                    <p><strong>– Reutilização de metais</strong></p>
+                    <p>Através da logística reversa é possível reutilizar os metais que estão presentes nesses materiais e reciclá-los!</p>
+                    <p><strong>– Proteção de dados</strong></p>
+                    <p>Com o descarte incorreto você corre o risco de ter os seus dados roubados e compartilhados por terceiros.</p>
+                    <p><strong>– Impulso à economia</strong></p>
+                    <p>Ainda através da logística reversa é possível e de forma sustentável movimentar a economia ao reutilizar materiais que iriam para o lixo.</p>
+                    <p><strong>– Conservação solo e da água</strong></p>
+                    <p>Ao descartar o lixo eletrônico da sua empresa de modo inadequado o mesmo poderá liberar substâncias tóxicas no solo e na água.</p>
+                </ul>
+            </div>
+            <br>
+            <span class="img-container"><img src="../img/a_natureza_agradece.webp" alt=""></span>
+            <p>Esperamos que esse post possa ter te ajudado, mas caso ainda tenha dúvidas sobre o descarte de lixo eletrônico, <a href="https://wa.me/+5543996915903">entre em contato conosco</a>! A FAVAI-Tech está aqui para te ajudar.</p>
+        </article>
+    </main>
     
 </body>
 </html>

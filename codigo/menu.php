e1d4350e959b3d96566b66a862b6cec794d6e575
Alexiafern <alexiafernanda718@gmail.com>
2023-03-09 09:39:58 -0300

    Adicionando descarte_de_lixo.php

--- codigo/menu.php
+++ codigo/menu.php
@@ -42,7 +42,10 @@
                             <ul class="nav navbar-nav"> 
                                 <!--Mudar ajuda.php para favai.php-->                           
                             <li>
-                                <a href="Ajuda.php">-----<img src="../img/WhatsApp Image 2023-02-15 at 19.11.47.png" alt=""width="35px">-----<br>Como Descartar <br>Seu Lixo?</a>
+                                <a href="Descarte_de_lixo.php">-----<img src="../img/WhatsApp Image 2023-02-15 at 19.11.47.png" alt=""width="35px">-----<br>Como Descartar <br>Seu Lixo?</a>
+                            </li>
+                            <li>
+                                <a href="Ajuda.php">-----<img src="../img/WhatsApp Image 2023-02-15 at 19.11.47.png" alt=""width="35px">-----<br>Como Descartar <br>Seu E-Lixo?</a>
                             </li>
                             <li>
                                 <a href="meio_ambiente.php">-----<img src="../img/WhatsApp Image 2023-02-15 at 19.11.47.png" alt=""width="35px">-----<br>Meio <br> Ambiente</a>

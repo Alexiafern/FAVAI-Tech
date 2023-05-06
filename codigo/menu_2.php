2aaeb95ce5e43505a9dcd0bf4c5974906a47e3bd
Alexia Fernanda <alexiafernanda718@gmail.com>
2023-05-06 18:34:12 -0300

    blabla

---
+++ codigo/menu_2.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+  <link rel="stylesheet" href="../estilo/menu_2.css">
+  <title>Comprar</title>
+</head>
+
+<body>
+  <nav class="slide">
+    <input type="radio" name="slideItem" id="item-1" class="toggle" checked />
+    <label for="item-1">
+      <a href="#">
+        <p class="icon">♬</p><span>Home</span>
+      </a>
+    </label>
+    <input type="radio" name="slideItem" id="item-2" class="toggle" />
+    <label for="item-2">
+      <a href="comprar.php">
+        <p class="icon">★</p><span>Comprar</span>
+      </a>
+    </label>
+    <input type="radio" name="slideItem" id="item-3" class="toggle" />
+    <label for="item-3">
+      <a href="doacao.php">
+        <p class="icon">✈</p><span>Doar</span>
+      </a>
+    </label>
+    <input type="radio" name="slideItem" id="item-4" class="toggle" />
+    <label for="item-4">
+      <p class="icon">✎</p><span>Produtos</span>
+    </label>
+    <div class="clear"></div>
+    <div class="slider">
+      <div class="bar"></div>
+    </div>
+  </nav>
+
+</body>
+
+</html>
